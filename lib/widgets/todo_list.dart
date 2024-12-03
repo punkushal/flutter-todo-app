2faf61e0c5b9ea522497a5b48090bfc2771f8b3f
punkushal <spycymagar@gmail.com>
2024-12-03 15:30:02 +0545

    For Displaying All Todo List

---
+++ lib/widgets/todo_list.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/providers/todo_provider.dart';
+
+import 'todo_item.dart';
+
+class TodoList extends StatelessWidget {
+  const TodoList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<TodoProvider>(context);
+    return ListView.builder(
+      itemCount: provider.allTodos.length,
+      itemBuilder: (ctx, index) => TodoItem(
+        todo: provider.allTodos[index],
+      ),
+    );
+  }
+}
